45ab7cffe68b5a4554a8f1650b034532e89a6273
Muhammet AYAN <muhammet.ayan@hotmail.com>
2019-02-17 23:27:34 +0300

    DBConnect içinde dbmodel yapısı test edildi

--- Data/DBConnect.py
+++ Data/DBConnect.py
@@ -1,4 +1,5 @@
 import pyodbc
+from Data.DbTestModel import *
 cnxn = pyodbc.connect("Driver={SQL Server};"
                       "Server=.;"
                       "Database=testDB;"
@@ -7,15 +8,28 @@ cnxn = pyodbc.connect("Driver={SQL Server};"
                       "Password=12345678;"
                       )
 """Provider=SQLOLEDB.1;Password=12345678;Persist Security Info=True;User ID=user;Initial Catalog=testDB;Data Source=."""
-
-cursor = cnxn.cursor()
 # cursor.execute('CREATE TABLE Test1(id int, name varchar(50));')
-cnxn.commit()  # değişiklikleri kaydediyor.
 # cursor.execute("DROP TABLE Test1;")
-cursor.execute('Select * from ValueTable')
-for row in cursor:
-    for col in row:
-        print(col, end=", ")
-    print("")
-cnxn.commit()
-cursor.close()
+
+
+def query(sql: str):
+    cursor = cnxn.cursor()
+    cursor.execute(sql)
+    cnxn.commit()  # değişiklikleri kaydediyor.
+    cursor.close()
+
+
+def select(sql: str, dbModel):
+    cursor = cnxn.cursor()
+    cursor.execute(sql)
+    result = []
+    for row in cursor.fetchall():
+        # result.append(dbModel(row))
+        result.append(dbModel(row))
+    cursor.close()
+    return result
+
+
+x= select("select * from valueTable", dbTestModel)
+for i in x:
+    print(i.name)
---
+++ Data/DbTestModel.py
@@ -0,0 +1,5 @@
+class dbTestModel:
+
+    def __init__(self, data: tuple):
+        self.id = data[0]
+        self.name = data[1]
